be7d3613be3848f30dbf233fa0efe015bbdf2689
Darin Wallace <dwalleye66@gmail.com>
2021-02-04 10:01:45 -0600

    modified package.json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
   
 var express = require("express");
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3001;
 var app = express();
 var exphbs = require("express-handlebars");
 var routes = require("./controllers/burgers_controller.js");
